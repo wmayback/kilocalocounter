acc07aa66d7eb9e0ed1f5f56077196b9d40a9dc3
wmayback <wmayback@gmail.com>
2014-01-05 18:00:48 +0000

    first commit

---
+++ app/controllers/calorie_controller.rb
@@ -0,0 +1,3 @@
+class CalorieController < ApplicationController
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kilocalocounter::Application.config.secret_key_base = '219aea81864feb6b6132adf3ec2adbf1dd1e89f96eb4b41125126c58765e968c473e28812047835b103a1e292a96bc916932f7e751dd6a5b7ec2664ce443c5e7'
