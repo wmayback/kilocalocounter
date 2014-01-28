33ed3010fa871bc31429f9edb4b1e6fb8bc0eb0e
wmayback <wmayback@gmail.com>
2014-01-28 03:11:17 +0000

    inserted succesfully the day_total method into the view. problem i just realized is that the view only displays meal1 for all of the meal values. the view needs to be altered to dynamically show each individual meal's calories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
-      def dayfinder(day_name)
+  def dayfinder(day_name)
       return Day.find_by_name(day_name)
+    end
+ 
+    def day_total(day_of_the_week)
+      subject_day = Day.find_by_name(day_of_the_week)
+      total = subject_day.meal1 + subject_day.meal2 + subject_day.meal3 + subject_day.meal4 + subject_day.meal5 + subject_day.meal6 + subject_day.meal7 + subject_day.meal8 + subject_day.meal9 + subject_day.meal10
+      return total
     end
     
     helper_method :dayfinder
-  
+    helper_method :day_total
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery #with: :exception
