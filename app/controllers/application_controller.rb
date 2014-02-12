3f6d5b5948db799a56c77f9df1e13a793e776d12
wmayback <wmayback@gmail.com>
2014-02-12 03:18:55 +0000

    created day_arrayer to place respective meals in an array, then fooled around with the html to try to sort the columns better, a lot of work to do on that end. next try to piece together the excess fasting and lean numbers under the columns. Maybe make the week data like pounds lost projections and such float right on the top like the static data is floating left

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
-  def dayfinder(day_name)
+    def dayfinder(day_name)
       return Day.find_by_name(day_name)
     end
+
+  def day_arrayer(day_name)
+    day_to_array = Day.find_by_name(day_name)
+    day_arrayed = Array.new
+    day_arrayed[0] = day_to_array.meal1
+    day_arrayed[1] = day_to_array.meal2
+    day_arrayed[2] = day_to_array.meal3
+    day_arrayed[3] = day_to_array.meal4
+    day_arrayed[4] = day_to_array.meal5
+    day_arrayed[5] = day_to_array.meal6
+    day_arrayed[6] = day_to_array.meal7
+    day_arrayed[7] = day_to_array.meal8
+    day_arrayed[8] = day_to_array.meal9
+    day_arrayed[9] = day_to_array.meal10
+    return day_arrayed
+  end
  
     def day_total(day_of_the_week)
       subject_day = Day.find_by_name(day_of_the_week)
@@ -10,6 +26,7 @@ class ApplicationController < ActionController::Base
     end
     
     helper_method :dayfinder
+    helper_method :day_arrayer
     helper_method :day_total
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
