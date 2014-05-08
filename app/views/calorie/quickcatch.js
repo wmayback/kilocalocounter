c87ebec4e21b92deaab62a1bd08ea7c5bf61bd31
William Mayback <wmayback@William-Maybacks-MacBook-Pro.local>
2014-05-07 19:57:56 -0500

    extracted a lot of logic out of the updaters and created seperate functions for them, consolidated all monday updating to be within the function formally known as other day updater. two tracts of work on deck: one. create a function triggered by static data changes that updates all excess burn, cumulative fasting, and lean burn numbers, two. ensure that all following excess cumulative numbers update and correspond whenever a previous days data has changed

--- app/views/calorie/quickcatch.js
+++ app/views/calorie/quickcatch.js
@@ -1,7 +1,20 @@
-<script>
-$(document).ready(
+       var day = $(day_name + ' .weekday_name').val();
+       var cal1 = $(day_name + ' .meal1').val();
+       var cal2 = $(day_name + ' .meal2').val();
+       var cal3 = $(day_name + ' .meal3').val();
+       var cal4 = $(day_name + ' .meal4').val();
+       var cal5 = $(day_name + ' .meal5').val();
+       var cal6 = $(day_name + ' .meal6').val();
+       var cal7 = $(day_name + ' .meal7').val();
+       var cal8 = $(day_name + ' .meal8').val();
+       var cal9 = $(day_name + ' .meal9').val();
+       var cal10 = $(day_name + ' .meal10').val();
 
-var monday_updater = function() {
+
+function day_dissector(day_name)
+
+
+      var monday_updater = function() {
 
        var idle = $('#IDLE_BURN').val();
        var fast = $('#FASTING_FUELAGE').val();
@@ -24,58 +37,10 @@ var monday_updater = function() {
 
        $.ajax({url: '/my_week', type: 'POST', data: {"weekday_name": day, "meal1": cal1, "meal2": cal2, "meal3": cal3, "meal4": cal4, "meal5": cal5, "meal6": cal6, "meal7": cal7, "meal8": cal8, "meal9": cal9, "meal10": cal10 }});
 
-       var daytotal = Number(cal1)+Number(cal2)+Number(cal3)+Number(cal4)+Number(cal5)+Number(cal6)+Number(cal7)+Number(cal8)+Number(cal9)+Number(cal10)
+       var daytotal = Number(cal1)+Number(cal2)+Number(cal3)+Number(cal4)+Number(cal5)+Number(cal6)+Number(cal7)+Number(cal8)+Number(cal9)+Number(cal10);
 
        $("#DayTotal").text(daytotal);
        $("#ExcessBurn").text(Number(idle)-Number(daytotal));
        $("#FastingExcess").text(Number(fast)-Number(daytotal));
        $("#LeanExcess").text(Number(lean)-Number(daytotal));
-
 };
-
-var other_day_updater = (function(day_name) {
-
-       var idle = $('#IDLE_BURN').val();
-       var fast = $('#FASTING_FUELAGE').val();
-       var lean = $('#LEAN_FUELAGE').val();
-
-       var day = $(day_name + ' .weekday_name').val();
-       var cal1 = $(day_name + ' .meal1').val();
-       var cal2 = $(day_name + ' .meal2').val();
-       var cal3 = $(day_name + ' .meal3').val();
-       var cal4 = $(day_name + ' .meal4').val();
-       var cal5 = $(day_name + ' .meal5').val();
-       var cal6 = $(day_name + ' .meal6').val();
-       var cal7 = $(day_name + ' .meal7').val();
-       var cal8 = $(day_name + ' .meal8').val();
-       var cal9 = $(day_name + ' .meal9').val();
-       var cal10 = $(day_name + ' .meal10').val();
-
-       console.log( $(day_name + ' .meal1').val());
-       console.log( $(day_name + ' .weekday_name').val());
-
-       $.ajax({url: '/my_week', type: 'POST', data: {"weekday_name": day, "meal1": cal1, "meal2": cal2, "meal3": cal3, "meal4": cal4, "meal5": cal5, "meal6": cal6, "meal7": cal7, "meal8": cal8, "meal9": cal9, "meal10": cal10 }});
-
-       var daytotal = Number(cal1)+Number(cal2)+Number(cal3)+Number(cal4)+Number(cal5)+Number(cal6)+Number(cal7)+Number(cal8)+Number(cal9)+Number(cal10)
-       var previous_fasting_excess = $('#Monday #FastingExcess').text();
-       var previous_lean_excess = $('#Monday #LeanExcess').text();
-
-       $(day_name + " #DayTotal").text(daytotal);
-       $(day_name + " #ExcessBurn").text(Number(idle)-Number(daytotal));
-       $(day_name + " #FastingExcess").text(Number(fast)+Number(previous_fasting_excess)-Number(daytotal));
-       $(day_name + " #LeanExcess").text(Number(lean)+Number(previous_lean_excess)-Number(daytotal));
-};
-
-
-
-      $('#Monday > tr').change(monday_updater() {});
-      $('#Tuesday > tr').change(other_day_updater('#Tuesday') {});
-      $('#Wednesday > tr').change(other_day_updater('#Wednesday') {});
-      $('#Thursday > tr').change(other_day_updater('#Thursday') {});
-      $('#Friday > tr').change(other_day_updater('#Friday') {});
-      $('#Saturday > tr').change(other_day_updater('#Saturday') {});
-      $('#Sunday > tr').change(other_day_updater('#Sunday') {});
-
-)
-
-</script>
